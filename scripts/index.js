3db3045d2a76059380eace912b0e0f3e0a231fa9
tahmina-nishu <akthertahmina572@gmail.com>
2024-10-22 20:23:15 +0600

    added like handeler

--- scripts/index.js
+++ scripts/index.js
@@ -2,6 +2,7 @@
         --------------------------OVERVIEW--------------------------
 
         1. Created a function "loadCategories()" to fetch all categories
+
         2. Created a function "displayCategories()" to display all category as a button
             -> passed 'categories' as argument
             -> created a 'container' by the id where(in HTML file) the categories will show
@@ -9,17 +10,22 @@
             -> in this loop created a div
             -> in this div set innerhtml of button
             -> child(new created div) is appened in parent (container)
+
         3. Created a function "loadPets()" as same as the function "loadCategories()" to fetch all pets
+
         4. Created a function "displayPets()" as the function "displayCategories()"  to display all pets in card
+       
         5. Created a function "loadCategoryPets()" to display pets by category
             -> passed "category name" as parameter
             -> Added onclick handler at the 'button' in the funcion "displayCategories()" and passed "category name" as argument
             -> called "displayPets()" function
+       
         6. shows a error msg if there is no content in the selected category
            for this ---> in the function "displayPets()"
                 -> check if the innerHTML of the container is empty or not
                 -> if it's empty then remove 'grid' class and set the content which will show on the screen
                    else add 'grid' class
+        
         7. to show the button is active-----
             -> set a "id" at the button in "displayCategories()" function
             -> go to "loadCategoryPets()" function
@@ -27,11 +33,38 @@
                 -> set css style at "activeClass" class
                 -> add "activeClass" class for active
             -> then call the "displayPets()" function
-        8.Remove the active button
+
+        8. Remove the active button
             -> created a function named "removeActiveClass()"
             -> set a 'class' at the button
             -> then in a 'for-of' loop remove "activeClass" class
 
+        9. Show the pet details
+            -> set a 'onclick' handeler at "Details" button named "loadDetails(${pet.petId})" here 'pet.petId' is the pet id which is the argument
+            -> create a function named "loadDetails()" and set a parameter
+            -> fetch the single pets Api (Ami ekhane async function use korchi)
+
+        10. create a function named "showDetails()" and set a parameter
+
+        11. get a "modal" from 'DaisyUi'
+
+        12. set a id in modal button
+
+        13. clear all content from modal and create a div with a Id
+
+        14. in the function "showDetails()"
+            -> get the id of the modal content
+            -> set the inner html of modal
+            -> get the modal button id and add "click()" handler
+
+        15. Handle like button
+            -> set class "h-fit" in selected pets div in html 
+            -> create a function named "loadLikedPets()" and fetch the pet from API
+            -> create a function named "addLikedPets()"
+                -> get the container id
+                -> create a div to add image
+                -> set inner html 
+                -> append the chiild to the container
 
 */
 
@@ -112,11 +145,11 @@ const displayPets = (pets) => {
                 <p class="text-[#131313B3]">Price  : ${pet.price} $</p>
 
                 <div class="card-actions">
-                    <button class="px-4 py-1 rounded-lg border-2 border-[#0E7A8126] text-[#0E7A81] font-bold"><img class="w-10" src="https://static.vecteezy.com/system/resources/previews/021/013/524/original/like-icon-on-transparent-background-free-png.png"></button>
+                    <button onclick="loadLikedPets(${pet.petId})" class="px-4 py-1 rounded-lg border-2 border-[#0E7A8126] text-[#0E7A81] font-bold"><img class="w-10" src="https://static.vecteezy.com/system/resources/previews/021/013/524/original/like-icon-on-transparent-background-free-png.png"></button>
 
                     <button class="px-4 py-1 rounded-lg border-2 border-[#0E7A8126] text-[#0E7A81] font-bold">Adopt</button>
 
-                    <button class="px-4 py-1 rounded-lg border-2 border-[#0E7A8126] text-[#0E7A81] font-bold">Details</button>
+                    <button onclick="loadDetails(${pet.petId})" class="px-4 py-1 rounded-lg border-2 border-[#0E7A8126] text-[#0E7A81] font-bold"> Details </button>
                 </div>
 
             </div>
@@ -146,6 +179,70 @@ const removeActiveClass = () => {
     for(let btn of buttons){
        btn.classList.remove('activeClass');
     }
+}
+
+const loadDetails = async (petId) =>{
+    console.log(petId);
+
+    const uri = `https://openapi.programming-hero.com/api/peddy/pet/${petId}`;
+    const res = await fetch(uri);
+    const data = await res.json();
+    showDetails(data.
+        petData);
+}
+
+const showDetails = (pets) => {
+    console.log(pets);
+    const detailsContainer = document.getElementById('modal-content');
+
+    detailsContainer.innerHTML = `
+    
+        <img class="w-[350px] mx-auto rounded-md" src="${pets.image}">
+
+        <div class="my-6">
+            <h2 class="text-xl font-bold">${pets.pet_name}</h2>
+
+            <div class="text-[#131313B3] flex gap-9 border-b pb-4">
+                <div>
+                    <p>Breed  : ${pets.breed}</p>
+                    <p>Gender : ${pets.gender}</p>
+                    <p>Vaccinated status : ${pets.vaccinated_status}</p>
+                </div>
+                <div>
+                    <p>Birth  : ${pets.date_of_birth}</p>
+                    <p>Price  : ${pets.price} $</p>
+                </div>
+            </div>
+        </div>
+
+        <div>
+            <h3 class="text-lg font-bold"> Details Information </h3>
+            <p class="text-[#131313B3]"> ${pets.pet_details}</p>
+        </div>
+
+    `;
+
+    document.getElementById('showModalData').click();
+}
+
+const loadLikedPets = async (petId) =>{
+    console.log(petId);
+
+    const uri = `https://openapi.programming-hero.com/api/peddy/pet/${petId}`;
+    const res = await fetch(uri);
+    const data = await res.json();
+    addLikedPets(data.
+        petData);
+}
+
+const addLikedPets = (pet) => {
+    const imageContainer = document.getElementById('selected-pets');
+    
+    const likedPet = document.createElement('div');
+        likedPet.innerHTML = `
+        <img src="${pet.image}">
+        `;
+    imageContainer.appendChild(likedPet);
 }
 
 loadCategories();
